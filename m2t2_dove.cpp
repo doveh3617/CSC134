ea9bf24ebd5c62d250775ac3d5bcdb7198c8c083
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-02-09 20:01:08 +0000

    m2t2 done

--- m2t2_dove.cpp
+++ m2t2_dove.cpp
@@ -13,7 +13,14 @@ int main()
     int num_apples;
     double price_each;
 
-    cout << "Welcome to the " << name << " apple farm!" << endl;
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << "How many apples are for sale?" << endl;
+    cin >> num_apples;
+    cout << "How much per apple?" << endl;
+    cin >> price_each;
+
+    cout << "Welcome to the " << name << "'s apple farm!" << endl;
     cout << "We have " << num_apples << " apples in stock." << endl;
     cout << "Each apple costs $" << price_each << "." << endl;
 
@@ -22,6 +29,7 @@ int main()
     cout << "The total price for all " << num_apples;
     cout << " apples is: $" << total_price << endl;
     cout << "Thanks for coming by!" << endl;
+
 
     return 0;
 }
