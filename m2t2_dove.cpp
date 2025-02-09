d03ae300bdf151a6bf709af9d55981b741e77e28
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-02-09 19:43:54 +0000

    m2t2 change variables

---
+++ m2t2_dove.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M2T1
+// Hayley Dove
+// 02/09/2025
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int num_apples;
+    double price_each;
+
+    cout << "Welcome to the " << name << " apple farm!" << endl;
+    cout << "We have " << num_apples << " apples in stock." << endl;
+    cout << "Each apple costs $" << price_each << "." << endl;
+
+    double total_price = num_apples * price_each;
+
+    cout << "The total price for all " << num_apples;
+    cout << " apples is: $" << total_price << endl;
+    cout << "Thanks for coming by!" << endl;
+
+    return 0;
+}
