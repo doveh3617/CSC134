3e12e4b48b89b9907e60570f85ab5c0028d3e964
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-02-09 20:18:50 +0000

    m1Lab done

---
+++ M1Lab_Dove.cpp
@@ -0,0 +1,30 @@
+// CSC 134
+// M2T1
+// Hayley Dove
+// 02/09/2025
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Hayley Dove";
+    int num_apples = 100;
+    double pricePerApple = 0.25;
+
+    double totalPrice = (double) num_apples * pricePerApple;
+
+    cout << "Welcome to " << name; 
+    cout << "'s apple orchard!" << endl;
+    cout << "We have " << num_apples;
+    cout << " apples in stock." << endl;
+    cout << "Apples are currently $";
+    cout << pricePerApple << " each." << endl;
+
+
+    cout << "If you want them all, that will be $";
+    cout << totalPrice << endl;
+
+    return 0;
+}
