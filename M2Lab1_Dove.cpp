cd3a1a177fbd8062ba1b5db42089b1bb26a0f2c4
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-02-17 02:44:20 +0000

    M2Lab1 Done

--- M2Lab1_Dove.cpp
+++ M2Lab1_Dove.cpp
@@ -1,10 +1,52 @@
-#include <iostream>
+// CSC 134
+// M2Lab1
+// Hayley Dove
+// 02/16/2025
 
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+    // Constants for cost and amount charged
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+    // Variables
+    double length, // Crate's length
+            width, // Crate's width
+            height, // Crate's height
+            volume, // Crate's volume
+            cost, // Cost to build
+            charge, // Charge to customer
+            profit; // Profit made
+
+    // Set desired output formatting
+    cout << setprecision(2) << fixed << showpoint;
     
+    // Prompt user for the crate's length, width, and height
+    cout << "Enter the dimensions of the crate (in feet):\n";
+    cout << "Length: ";
+    cin >> length;
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: ";
+    cin >> height;
+
+    // Calculate crate volume, production cost, charge to customer
+    // and profit
+    volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost;
+
+    // Display calculations
+    cout << "The volume of the crate is ";
+    cout << volume << " cubic feet.\n";
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to customer: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
 
     return 0;
 }
