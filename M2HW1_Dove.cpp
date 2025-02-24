876c95e3d1ae2c7f4d9d1c9276eb8377934b2ce6
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-02-24 03:49:27 +0000

    m2hw1 done

--- M2HW1_Dove.cpp
+++ M2HW1_Dove.cpp
@@ -4,10 +4,35 @@
 // 02/23/2025
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-    
+    // declare variables
+    string firstName, lastName;
+    double startBal, deposit, withdrawal, endBal;
+    int accountNum = 56318739;
+
+    // Set desired output formatting
+    cout << setprecision(2) << fixed << showpoint;
+
+    // ask user for input
+    cout << "Please enter your name." << endl;
+    cin >> firstName >> lastName;
+    cout << "What is your starting account balance?" << endl;
+    cin >> startBal;
+    cout << "How much do you want to deposit?" << endl;
+    cin >> deposit;
+    cout << "How much do you want to withdraw?" << endl;
+    cin >> withdrawal;
+
+    // calculate ending balance
+    endBal = startBal + deposit - withdrawal;
+
+    // output banking information
+    cout << firstName + " " + lastName << endl;
+    cout << accountNum << endl;
+    cout << "$" << endBal << endl;
 
     return 0;
 }
