5c1814d04c021a1cb31ae16bf39d28d6cdd6bf31
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-02-24 04:10:53 +0000

    m2hw1 q4 start

---
+++ m2hw1_q4_Dove.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M2HW1 Q3
+// Hayley Dove
+// 02/23/2025
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declare variables
+    string letsGo, school, team, cheerOne, cheerTwo;
+    letsGo = "Let's go";
+    school = "FTCC";
+    team = "Trojans";
+    cheerOne = letsGo + " " + school;
+    cheerTwo = letsGo + " " + team;
+    
+
+    return 0;
+}
