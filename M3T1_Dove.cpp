5baf0c34f951df0119629ab33e32999bf0003a48
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-02-24 02:53:21 +0000

    m3t1 start

---
+++ M3T1_Dove.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M3T1
+// Hayley Dove
+// 02/23/2025
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // declare variables
+
+
+    // ask user for input
+
+    // calculate answer
+
+    // display results
+
+
+    return 0;
+}
