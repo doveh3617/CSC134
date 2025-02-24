a489c7df8b998a5093a43fa181bdad7e6cad4bbb
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-02-24 03:01:57 +0000

    m3t1 working

--- M3T1_Dove.cpp
+++ M3T1_Dove.cpp
@@ -4,20 +4,28 @@
 // 02/23/2025
 
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
+int main() {
     // declare variables
-
+    int lengthOne, widthOne, lengthTwo, widthTwo;
+    int areaOne, areaTwo;
 
     // ask user for input
-
+    cout << "Please enter the length and width of the first rectangle, \n";
+    cout << "separated by a space or new line." << endl;
+    cin >> widthOne >> lengthOne;
+    cout << "Please enter the length and width of the second rectangle, \n";
+    cout << "separated by a space or new line." << endl;
+    cin >> widthTwo >> lengthTwo;
+    
     // calculate answer
+    areaOne = widthOne * lengthOne;
+    areaTwo = widthTwo * lengthTwo;
 
     // display results
-
+    cout << "The area of the first rectangle is: " << areaOne << endl;
+    cout << "The area of the second rectangle is: " << areaTwo << endl;
 
     return 0;
 }
