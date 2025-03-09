381f9166f786cb01a6a909ba0265eee58a9cc7e5
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-03-09 18:37:58 +0000

    m3hw1 Q1

--- m3HW1_Dove.cpp
+++ m3HW1_Dove.cpp
@@ -9,6 +9,25 @@ using namespace std;
 
 int main() {
 
+    string choice;
+
+    // Question 1
+    cout << "Question 1" << endl;
+    cout << "Hello, I am a C++ program!" << endl;
+    cout << "Do you think that I am helpful? Please type yes or no" << endl;
+    cin >> choice;
+
+    if (choice == "yes") {
+        cout << "Glad to be useful to you!" << endl;
+    }
+    else if (choice == "no") {
+        cout << "Maybe one day you will understand my capabilities." << endl;
+    }
+    else {
+        cout << "This wasn't a choice given, I understand if you feel indifferent about me." << endl;
+    }
+
+    
 
   return 0;
 
