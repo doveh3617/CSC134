3bcad38954e6d2a8fb028473a1db9e4c3bd79b49
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-03-09 19:21:02 +0000

    m3HW1 Q2

--- m3HW1_Dove.cpp
+++ m3HW1_Dove.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 using namespace std;
 
+void chooseFire;
+void chooseSwim;
 
 int main() {
 
@@ -27,7 +29,58 @@ int main() {
         cout << "This wasn't a choice given, I understand if you feel indifferent about me." << endl;
     }
 
+    // Question 2
+    int decision;
+    double price;
+    double tipAmt;
+    double taxAmt;
+    double totalWithTax;
+    double total;
+    const double TIP = .15;
+    const double TAX = .07;
+
+    cout << "Question 2" << endl;
+    cout << "Welcome to the steakhouse!" << endl;
+    cout << "Will your order be dine in or togo? Choose 1 for dine-in and 2 for togo." << endl;
+    cin >> decision;
+
+    cout << "Please enter the price of your meal." << endl;
+    cin >> price;
     
+    if (decision == 1) {
+        taxAmt = price * TAX;
+        totalWithTax = price + taxAmt;
+        tipAmt = price * TIP;
+        total = totalWithTax + tipAmt;
+
+        cout << "Your meal price was: $" << price << endl;
+        cout << "Your tax amount was: $" << taxAmt << endl;
+        cout << "Your tip amount was: $" << tipAmt << endl;
+        cout << "Your total amount was: $" << total << endl;
+
+    }
+    else if (decision == 2) {
+        tipAmt = 0;
+        taxAmt = price * TAX;
+        total = price * taxAmt;
+        cout << "Your meal price was: $" << price << endl;
+        cout << "Your tax amount was: $" << taxAmt << endl;
+        cout << "Your tip amount was: $" << tipAmt << endl;
+        cout << "Your total amount was: $" << total << endl;
+    }
+    else {
+        cout << "That is not a valid choice." << endl;
+    }
+
+    // Question 3
+    int option;
+    cout << "Question 3" << endl;
+    cout << "You are stranded on a deserted island, you can either build a fire or go for a swim." << endl;
+    cout << "Which option will you choose? Please use 1 for fire or 2 for swim." << endl;
+    cin >> option;
+
+    
+
 
   return 0;
 
