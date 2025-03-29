4a2b64b697baed24acc5c2f816962fb8ab429990
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-03-29 22:23:33 +0000

    m4Lab1 commit

--- M4Lab1_Dove.cpp
+++ M4Lab1_Dove.cpp
@@ -4,10 +4,24 @@
 // 03/29/2025
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    
+    int height;
+    int width;
+
+    cout << "How tall should the block be?" << endl;
+    cin >> height;
+    cout << "How wide should the block be?" << endl;
+    cin >> width;
+
+    for (int j=0; j < height; j++) {
+        for (int i=0; i < width; i++) {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
