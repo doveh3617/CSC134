a443c919db503a15b03a46cabe99ba2a9f11ea6b
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2025-04-06 21:45:49 +0000

    M5T2 working

--- M5T2_Dove.cpp
+++ M5T2_Dove.cpp
@@ -6,10 +6,23 @@
 #include <iostream>
 using namespace std;
 
+void printResult(int first, int second) {
+    cout << first << " squared = " << second << endl;
+
+}
+
 
 
 int main()
 {
+    int count = 1;
+    int result;
+
+    while (count <= 10) {
+        result = count * count;
+        printResult(count, result);
+        count++;
+    }
     
     return 0;
 }
